27bff2b06f38865e4a5ca26ccff18f6d29c99e31
Chris Beard <cbeard@hnoj.org>
2021-08-18 11:11:45 -0500

    Adding the model

---
+++ models/Workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => Date.now()
+        },
+        exercise: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter an exercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter an exercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Please enter a duration in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    }
+);
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
